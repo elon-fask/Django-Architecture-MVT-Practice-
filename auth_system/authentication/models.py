a512a770bfedf94ae80485afcd89e0407bc01a2b
harvesting <harvesting@localhost.localdomain>
2024-09-26 20:16:01 +0100

    init django project

---
+++ auth_system/authentication/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# User model is already provided by Django, so no custom model is required here
+from django.contrib.auth.models import User
+
+
+# Create Custom models here.
