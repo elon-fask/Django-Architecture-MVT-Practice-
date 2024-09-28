40750b5bd498f528f515e71aa7d61a6fbc3926cf
harvesting <harvesting@localhost.localdomain>
2024-09-28 20:26:21 +0100

    models.py using django.User models as Abstract

--- auth_system/authentication/models.py
+++ auth_system/authentication/models.py
@@ -1,7 +1,5 @@
+# import required modules
 from django.db import models
 
 # User model is already provided by Django, so no custom model is required here
-from django.contrib.auth.models import User
-
-
-# Create Custom models here.
+from django.contrib.auth.models import AbstractUser, User
